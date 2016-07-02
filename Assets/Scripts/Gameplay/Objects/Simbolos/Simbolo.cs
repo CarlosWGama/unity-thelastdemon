efafc5861c9e70bcb101ca4200f4cac368406065
Carlos W. Gama <carloswgama@gmail.com>
2016-07-01 21:34:01 -0300

    - Level 16 (Limbo) - Velas piscando - Velas com particulas

--- Assets/Scripts/Gameplay/Objects/Simbolos/Simbolo.cs
+++ Assets/Scripts/Gameplay/Objects/Simbolos/Simbolo.cs
@@ -3,9 +3,46 @@
 public class Simbolo : MonoBehaviour, ISimbolo {
 
     private LevelController levelController;
+    private SpriteRenderer imagem;
+
+    /// <summary> Verifica se está surgindo ou desaparecendo </summary>
+    private bool isFadeIn = false;
+
+    /// <summary> Duração de um fade in e ou </summary>
+    [SerializeField]
+    private float duration = 1f;
+
+    /// <summary> Controlador do efeito </summary>
+    private float timeController = 0f;
+
+    void Awake() {
+        imagem = GetComponent<SpriteRenderer>();
+    }
 
     void Start() {
         levelController = FindObjectOfType<LevelController>();
+    }
+
+
+    void Update() {
+        var color = imagem.color;
+        timeController += TimeManager.DeltaTime;
+        
+        var percentage = ((timeController * 100) / duration) / 100;
+        Debug.Log(timeController + " - " + percentage);        
+        percentage = Mathf.Clamp(percentage, 0, 1f);
+        
+
+        if (isFadeIn) color.a = (percentage);   //Fade IN
+        else color.a = (1f - percentage);        //Fade Out
+
+        if (percentage >= 1) {
+            isFadeIn = !isFadeIn; //Toggle
+            timeController = 0f;
+        }
+        
+
+        imagem.color = color;
     }
 
 	void OnTriggerEnter2D(Collider2D colisor) {
